5fe9d5e3a1739cee9b28f507f3c272f558999cbf
Nikola Perisic <cathode.y2k@gmail.com>
2019-06-27 17:58:00 +0200

    gallery image hover captions

--- client/src/Gallery/Gallery.js
+++ client/src/Gallery/Gallery.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.scss";
 
 const Gallery = props => {
+    const [show, setShow] = useState({ hover: false, name: "" });
+    const handleMouseEnter = name => {
+        setShow({ hover: true, name: name });
+    };
+    const handleMouseLeave = () => {
+        setShow({ hover: false, name: "" });
+    };
     return (
         <div className="gallery">
             {props.imgs.map(el => {
                 return (
-                    <div key={el.name}>
+                    <div
+                        className="img-wrap"
+                        key={el.name}
+                        onMouseEnter={() => handleMouseEnter(el.name)}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        {show.hover && show.name === el.name ? (
+                            <div>
+                                <p>{el.name}</p>
+                                <p>{el.author}</p>
+                            </div>
+                        ) : null}
                         <img
                             src={`http://localhost:5000${el.uri}`}
                             alt={el.name}
